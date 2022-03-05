be65712e21a7fdbc3e4bfd5d2a4d6501f0bd2e8d
Xazeq <setezey@gmail.com>
2022-03-05 09:40:40 +0300

    init

---
+++ src/main/java/ru/job4j/job4j_weather_reactive/Job4jWeatherReactiveApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.job4j_weather_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jWeatherReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jWeatherReactiveApplication.class, args);
+    }
+
+}
